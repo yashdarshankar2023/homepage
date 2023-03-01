2ba262f6759f93bf750fe592151cac166abb1170
yourusername <email@youremail.com>
2023-03-01 18:48:14 +0530

    fourth commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,11 +4,12 @@ import "../styles/navbar.css"
 const navbar = () => {
   return (
     <div className='navbar'>
+      <h1>HM</h1>
       <p>Home</p>
       <p>Service</p>
       <p>HM store</p>
       <p>About Us</p>
-      <p>Contact</p>
+      <p>Contact Us</p>
       
     </div>
   )
